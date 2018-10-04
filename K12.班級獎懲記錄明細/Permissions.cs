4f1f9bebf743db76573a2d6c218cd1fc24b17004
Vicky Yang <gaungshi@gmail.com>
2018-10-04 14:45:37 +0800

    2018/10/04 [ischoolKingdom] Vicky 作業 完成 [H成績][H學務][06] 功能沒有設定權限管理 項目

--- K12.班級獎懲記錄明細/Permissions.cs
+++ K12.班級獎懲記錄明細/Permissions.cs
@@ -7,7 +7,17 @@ namespace K12.ClassMeritDemerit.Detail
 {
     class Permissions
     {
-        public static string 班級學生獎懲明細 { get { return "Report0230"; } }
+        public static string 班級學生獎懲明細_新版 { get { return "Report0230"; } }
+
+        public static bool 班級學生獎懲明細權限_新版
+        {
+            get
+            {
+                return FISCA.Permission.UserAcl.Current[班級學生獎懲明細_新版].Executable;
+            }
+        }
+
+        public static string 班級學生獎懲明細 { get { return "625B8AE1-C581-4214-8E18-426DEF81D4DE"; } }
 
         public static bool 班級學生獎懲明細權限
         {
--- K12.班級獎懲記錄明細/Program.cs
+++ K12.班級獎懲記錄明細/Program.cs
@@ -13,14 +13,17 @@ namespace K12.ClassMeritDemerit.Detail
         public static void Main()
         {
             RibbonBarItem classSpecialItem = K12.Presentation.NLDPanels.Class.RibbonBarItems["資料統計"];
-            classSpecialItem["報表"]["學務相關報表"]["班級學生獎懲明細(新版)"].Enable = Permissions.班級學生獎懲明細權限;
+            classSpecialItem["報表"]["學務相關報表"]["班級學生獎懲明細(新版)"].Enable = Permissions.班級學生獎懲明細權限_新版;
             classSpecialItem["報表"]["學務相關報表"]["班級學生獎懲明細(新版)"].Click += delegate
             {
                 new K12.ClassMeritDemerit.Detail.Report().Print();
             };
 
             Catalog ribbon = RoleAclSource.Instance["班級"]["報表"];
+            // 2018.09.27 [ischoolKingdom] Vicky依據 [H成績][H學務][06] 功能沒有設定權限管理 項目，將各功能按鈕註冊時Enable設定與系統權限綁定，權限Code使用GUID。
             ribbon.Add(new RibbonFeature(Permissions.班級學生獎懲明細, "班級學生獎懲明細"));
+            //ribbon.Add(new RibbonFeature(Permissions.班級學生獎懲明細, "班級學生獎懲明細"));  
+            ribbon.Add(new RibbonFeature(Permissions.班級學生獎懲明細_新版, "班級學生獎懲明細(新版)")); //增加"(新版)"文字
         }
     }
 }
